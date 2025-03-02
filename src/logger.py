4873d4b413789aa27b8cb05a7c1a8afcab5aa20a
Shivam Sahni <shivamsahni507@gmail.com>
2025-03-02 22:25:00 +0530

    adding the exception handling

---
+++ src/logger.py
@@ -0,0 +1,21 @@
+import logging
+import os
+from datetime import datetime
+# used to track every expection inside of a file
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# this will create a log file with every folder implementation
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+# for logging we need to setup the basicConfig to make it available for every folder
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+if __name__=="__main__":
+    logging.info("Logging has started")
